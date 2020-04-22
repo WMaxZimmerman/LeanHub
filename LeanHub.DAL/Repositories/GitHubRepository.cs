9c5de78d5a6363be4b19ed8bfe4980281074ce9f
Austin Suiter <asuiter@leantechniques.com>
2020-04-22 15:31:55 -0500

    Adds method for less code duplication

--- LeanHub.DAL/Repositories/GitHubRepository.cs
+++ LeanHub.DAL/Repositories/GitHubRepository.cs
@@ -5,10 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net;
-
+using System.Net.Http.Headers;
 
 namespace LeanHub.DAL.Repositories
 {
+    public class GitHubUser
+    {
+        public static AuthenticationHeaderValue GetAdminCredentials(string credentials)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(credentials);
+            return new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+        public static string GetApiAddress(string userName)
+        {
+            var address = "https://api.github.com/orgs/TestyMcTestOrg/memberships/"+userName;
+            return address;
+        }
+
+    }
     public class GitHubRepository
     {
         public static string HelloWorld()
@@ -18,31 +32,31 @@ namespace LeanHub.DAL.Repositories
 
         public static string AddUserToOrg(string name)
         {
-            using(var client = new HttpClient()) 
-            {
-                var byteArray = Encoding.ASCII.GetBytes("austinsuiter:th3056cr3w");
-                var address = "https://api.github.com/orgs/TestyMcTestOrg/memberships/"+name;
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                client.DefaultRequestHeaders.Add("User-Agent", "LeanHub");
-
-                var response = client.PutAsync(address, null).Result;
-                var content = response.Content.ReadAsStringAsync().Result;
-
-                return content;
-            }
-         
+            return MakeApiCall(name, "PUT");
         }
 
         public static string RemoveUserFromOrg(string name)
         {
+            return MakeApiCall(name, "DELETE");
+        }
+
+        public static string GetListOfUsers()
+        {
+            return MakeApiCall(null, "GET");
+        }
+
+        public static string MakeApiCall(string name, string method)
+        {
             using(var client = new HttpClient()) 
             {
-                var byteArray = Encoding.ASCII.GetBytes("UserName:Password");
-                var address = "https://api.github.com/orgs/TestyMcTestOrg/memberships/"+name;
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var address = GitHubUser.GetApiAddress(name);
+                client.DefaultRequestHeaders.Authorization = GitHubUser.GetAdminCredentials("UserName:Password");
                 client.DefaultRequestHeaders.Add("User-Agent", "LeanHub");
 
-                var response = client.DeleteAsync(address).Result;
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri(address);
+                request.Method = new HttpMethod(method);
+                var response = client.SendAsync(request).Result;
                 var content = response.Content.ReadAsStringAsync().Result;
 
                 return content;
